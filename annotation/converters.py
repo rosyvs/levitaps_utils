bfd0aae2e6910b22ce455c83fb56fd94b8155fbc
rosyvs <rosy.southwell@gmail.com>
2023-02-09 18:01:32 -0700

    add converters to and from pd table

--- annotation/converters.py
+++ annotation/converters.py
@@ -8,6 +8,57 @@ import pandas as pd
 # useful generic format is a table with
 # [uttID, speaker, utterance, start_sec, end_sec]
 
+# separate function to write to csv, tsv or ELAN compatible (ELAN interprets ALL commas as delimiter)
+
+def HHMMSS_to_sec(time_str):
+    """Get Seconds from timestamp string with milliseconds."""
+    if time_str.count(':')==2:
+        h, m, s = time_str.split(':')
+    elif time_str.count(':')==3:
+    # weird timestamps where there is a field followign seconds delimited by colon
+        h, m, s, u = time_str.split(':')
+        # determine whether ms field is in tenths or hundredths or thousandths by countng how many digits
+        if len(u)==1:
+            print('Weird time format detected - HH:MM:SS:tenths - please verify this is how you want the time interpreted')
+            ms = float(u)/10
+        elif len(u)==2: # hundredths
+            ms = float(u)/100
+        elif len(u)==3: # hundredths
+            ms = float(u)/1000
+        else:
+            print(f'input string format not supported: {time_str}')
+            return None
+        s = int(s)+ms
+    elif time_str.count(':')==1:
+        # print('missing HH from timestamp, assuming MM:SS')
+        m, s = time_str.split(':')
+        h=0
+    else:
+        print(f'input string format not supported: {time_str}')
+        return None
+    return int(h) * 3600 + int(m) * 60 + float(s) 
+
+def docx_scraped_tsv_to_table(ooona_file):
+    # ooona output is a table in a word docx, 
+    # for now manually copying this out and saving as tsv
+    # but the timestamp format is wrong
+    # input cols are SHOT	START	END	SPEAKER	DIALOGUE
+
+    with open(ooona_file) as in_file:
+        reader = csv.reader(in_file, delimiter="\t")
+        next(reader) # skip header
+        rows=[]
+        for i,line in enumerate(reader):
+            utt_ix, start_time, end_time, speaker, transcript = line
+            start_sec = HHMMSS_to_sec(start_time)
+            end_sec = HHMMSS_to_sec(end_time)
+            rows.append([speaker,transcript,start_sec,end_sec])
+    utt_table = pd.DataFrame(rows, columns=['speaker','transcript','start_sec','end_sec'])
+    return(utt_table)
+    # table = pd.read_csv(ooona_file, sep='\t')
+
+def molly_xlsx_to_table(xl_file):
+    pass
 
 def saga_to_table(saga_txt):
     # saga's own transcripts are txt given in the following format
@@ -47,19 +98,13 @@ def saga_to_table(saga_txt):
     utt_table = pd.DataFrame(rows, columns=['speaker','transcript','start_sec','end_sec'])
     return(utt_table)
 
-def HHMMSS_to_sec(time_str):
-    """Get Seconds from timestamp string with milliseconds."""
-    if time_str.count(':')==2:
-        h, m, s = time_str.split(':')
-    elif time_str.count(':')==3:
-        print('Weird time format detected - HH:MM:SS:tenths - please verify this is how you want the time interpreted')
-    # "human error" fix for files with : as Second/millisecond delimiter and tenths of a second only, some early transcripts (2021) used this format
-        h, m, s, ms = time_str.split(':')
-        s = int(s)+float(ms)/10
-    elif time_str.count(':')==1:
-        # print('missing HH from timestamp, assuming MM:SS')
-        m, s = time_str.split(':')
-        h=0
-    else:
-        print(f'input string format not supported: {time_str}')
-    return int(h) * 3600 + int(m) * 60 + float(s) 
+def table_to_ELAN_tsv(table:pd.DataFrame, path:str):
+    # write table to tsv compatible with ELAN import
+    table.to_csv(path, sep='\t')
+
+def table_to_standard_csv(table:pd.DataFrame, path:str):
+    # write table to standard csv format agreed upon by whole team
+
+    # TODO: convert times in seconds back to HH:MM:SS? 
+    # TODO: split utterances into sentences? 
+    pass
