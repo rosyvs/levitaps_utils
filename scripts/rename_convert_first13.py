f1a08444d82671ab917d93b7aa1971860416b71e
rosyvs <rosy.southwell@gmail.com>
2023-02-12 16:04:25 -0700

    remove extra dots

--- scripts/rename_convert_first13.py
+++ scripts/rename_convert_first13.py
@@ -18,7 +18,7 @@ srclist = rename_files_SID_to_FN(src_dir)
 dest_dir = '/Users/roso8920/Dropbox (Emotive Computing)/LEVI/human_transcripts/molly_transcripts/utt_labels/'
 os.makedirs(dest_dir, exist_ok=True)
 for f in srclist:
-    sessname = Path(f).stem
+    sessname = Path(f).stem.strip('.') # some have extra . at end of filename
     tbl = molly_xlsx_to_table(f)
     csvf = os.path.join(dest_dir, f'utt_labels_{sessname}.csv')
     table_to_utt_labels_csv(tbl,csvf)
